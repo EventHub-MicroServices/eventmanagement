b15f34037f288edb2356d642a5bc9195f311aeb2
iyas311 <iyas2458@gmail.com>
2026-03-30 22:16:41 +0530

    env changed

--- booking-service/database.py
+++ booking-service/database.py
@@ -12,10 +12,10 @@ if not SQLALCHEMY_DATABASE_URL:
     db_user = os.getenv("DB_USER")
     db_pass = os.getenv("DB_PASSWORD")
     db_host = os.getenv("DB_HOST", "localhost")
-    db_port = os.getenv("DB_PORT", "3306")
+    db_port = os.getenv("DB_PORT", "5432")
     db_name = os.getenv("DB_NAME", "booking_db")
     if db_user and db_pass:
-        SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+        SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
     else:
         raise ValueError("DATABASE_URL or granular DB variables are not set")
 
